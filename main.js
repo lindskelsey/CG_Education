33afc8389054480cbaa4e492fc2e79650755ec0b
lindskelsey <lindsaykelsey@gmail.com>
2017-09-20 17:55:59 -0500

    add javascript file

---
+++ main.js
@@ -0,0 +1,11 @@
+var teacherName = "Sally Jones"
+var department = "Physics"
+var ratingOne = 1
+var ratingTwo = 2
+var ratingThree = 3
+var avgRating = (ratingOne + ratingTwo + ratingThree)/3
+
+console.log("Teacher: " + teacherName)
+console.log("Department: " + department)
+console.log("Ratings: " + ratingOne + "," + ratingTwo + "," + ratingThree)
+console.log(avgRating)
