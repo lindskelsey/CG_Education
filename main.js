9d6e6112e7ce9a323a23e2390308cd7c8a4ccf1e
lindskelsey <lindsaykelsey@gmail.com>
2017-10-18 19:49:41 -0500

    update to prompt and alert

--- main.js
+++ main.js
@@ -19,13 +19,22 @@ function addTeacherRating(ratings,newRating) {
 var userRating = prompt("We would like for you to review . Please enter a rating between 0.0 - 5.0?");
 
 if (userRating >=0 && userRating <=5) {
-  teacherRating.push(userRating)
+  teacherRating.push(parseInt(userRating))
   var totalNewRating = 0;
     for (i = 0; i < teacherRating.length; i++) {
       totalNewRating += teacherRating[i];
     }
     var finalRating = (totalNewRating/ teacherRating.length);
     alert("Thanks for you review! " + teacherName + "'s average rating is now " + finalRating + " .")
+} else {
+  userRating = prompt("We would like for you to review . Please enter a rating between 0.0 - 5.0?")
+}
+
+
+
+if (userRating >=0 && userRating <=5) {
+  teacherRating.push(parseInt(userRating))
+    alert("Thanks for you review! " + teacherName + "'s average rating is now " + getAvgRating(teacherRating) + " .")
 } else {
   userRating = prompt("We would like for you to review . Please enter a rating between 0.0 - 5.0?")
 }
