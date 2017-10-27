74cc7ec38e3211050ada28f7d04f7de78c3dfd10
lindskelsey <lindsaykelsey@gmail.com>
2017-10-27 15:17:39 -0500

    added filterByDept function

--- course.js
+++
@@ -1,7 +0,0 @@
-var courses = [
-                [course1,department1]
-                [course2, department2]
-                [course3, department3]
-              ]
-
-function filterByDepartment(courseArray,department)
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+
+
 var teacherName = "Jennifer Honey";
-var department = "Physics";
-var teacherRating = [1,2,3];
+var department = "English";
+var teacherRating = [3.0,4.5,5.0];
 
 function getAvgRating(ratings) {
   var total = 0;
@@ -16,7 +18,7 @@ function addTeacherRating(ratings,newRating) {
   return totalRating;
 }
 
-var userRating = prompt("We would like for you to review . Please enter a rating between 0.0 - 5.0?");
+var userRating = prompt("We would like for you to review " + teacherName + "." + " Please enter a rating between 0.0 - 5.0?");
 
 
 if (userRating >=0 && userRating <=5) {
@@ -24,4 +26,24 @@ if (userRating >=0 && userRating <=5) {
     alert("Thanks for you review! " + teacherName + "'s average rating is now " + getAvgRating(teacherRating) + " .")
 } else {
   userRating = prompt("We would like for you to review . Please enter a rating between 0.0 - 5.0?")
+}
+
+
+var coursesListFull = [
+                ["course1","English"],
+                ["course2", "English"],
+                ["course3", "Math"],
+                ["course4", "Science"],
+                ["course5", "Science"],
+                ["course6", "Math"]
+              ];
+
+function filterByDepartment (coursesArray, selectDept) {
+  var courseList = [];
+  for (var i = 0; i < coursesArray.length; i++) {
+    if (coursesArray[i][1] == selectDept){
+      courseList.push(coursesArray[i][0]);
+    }
+  }
+    return courseList;
 }
